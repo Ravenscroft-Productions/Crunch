d53c3326f0428068052098e8e07dfc172aff0566
Ravenscroft-Productions <steven.print@btinternet.com>
2025-09-09 16:27:36 +0100

    Initialize Team ID and Start Spot for Players

--- Source/Crunch/Private/Framework/CGameMode.cpp
+++ Source/Crunch/Private/Framework/CGameMode.cpp
@@ -3,3 +3,47 @@
 
 #include "Framework/CGameMode.h"
 
+#include "EngineUtils.h"
+#include "GenericTeamAgentInterface.h"
+#include "GameFramework/PlayerStart.h"
+
+APlayerController* ACGameMode::SpawnPlayerController(ENetRole InRemoteRole, const FString& Options)
+{
+	APlayerController* NewPlayerController = Super::SpawnPlayerController(InRemoteRole, Options);
+	IGenericTeamAgentInterface* NewPlayerTeamInterface = Cast<IGenericTeamAgentInterface>(NewPlayerController);
+	FGenericTeamId TeamID = GetTeamIDForPlayer(NewPlayerController);
+	if (NewPlayerTeamInterface)
+	{
+		NewPlayerTeamInterface->SetGenericTeamId(TeamID);
+	}
+	NewPlayerController->StartSpot = FindNextStartSpotForTeam(TeamID);
+	return NewPlayerController;
+}
+
+FGenericTeamId ACGameMode::GetTeamIDForPlayer(const APlayerController* PlayerController) const
+{
+	static int PlayerCount = 0;
+	++PlayerCount;
+	return FGenericTeamId(PlayerCount % 2);
+}
+
+AActor* ACGameMode::FindNextStartSpotForTeam(const FGenericTeamId& TeamID)
+{
+	const FName* StartSpotTag = TeamStartSpotTagMap.Find(TeamID);
+	if (!StartSpotTag)
+	{
+		return nullptr;
+	}
+
+	UWorld* World = GetWorld();
+
+	for (TActorIterator<APlayerStart> It(World); It; ++It)
+	{
+		if (It->PlayerStartTag == *StartSpotTag)
+		{
+			It->PlayerStartTag = FName("Taken");
+			return *It;
+		}
+	}
+	return nullptr;
+}
