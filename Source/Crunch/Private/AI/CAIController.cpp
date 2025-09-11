79845e44eb1685afdae0cc9fbe810529f0a80a58
Ravenscroft-Productions <steven.print@btinternet.com>
2025-09-11 19:17:16 +0100

    Implement updated the Blackboard Target when the AI forgets an old Target

--- Source/Crunch/Private/AI/CAIController.cpp
+++ Source/Crunch/Private/AI/CAIController.cpp
@@ -25,6 +25,7 @@ ACAIController::ACAIController()
 
 	AIPerceptionComponent->ConfigureSense(*SightConfig);
 	AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ACAIController::TargetPerceptionUpdated);
+	AIPerceptionComponent->OnTargetPerceptionForgotten.AddDynamic(this, &ACAIController::TargetForgotten);
 }
 
 void ACAIController::OnPossess(APawn* NewPawn)
@@ -57,7 +58,16 @@ void ACAIController::TargetPerceptionUpdated(AActor* TargetActor, FAIStimulus St
 	}
 	else
 	{
-		if (GetCurrentTarget() == TargetActor) SetCurrentTarget(nullptr);
+	}
+}
+
+void ACAIController::TargetForgotten(AActor* ForgottenActor)
+{
+	if (!ForgottenActor) return;
+
+	if (GetCurrentTarget() == ForgottenActor)
+	{
+		SetCurrentTarget(GetNextPerceivedActor());
 	}
 }
 
@@ -84,5 +94,20 @@ void ACAIController::SetCurrentTarget(AActor* NewTarget)
 	{
 		BlackboardComponent->ClearValue(TargetBlackboardKeyName);
 	}
+}
+
+AActor* ACAIController::GetNextPerceivedActor() const
+{
+	if (PerceptionComponent)
+	{
+		TArray<AActor*> Actors;
+		AIPerceptionComponent->GetPerceivedHostileActors(Actors);
+
+		if (Actors.Num() != 0)
+		{
+			return Actors[0];
+		}
+	}
+	return nullptr;
 }
 	
