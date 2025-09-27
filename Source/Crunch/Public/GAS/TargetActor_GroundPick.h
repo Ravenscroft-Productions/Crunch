753d2e4cb018d51ffa29792f87e688f0a88f3187
Ravenscroft-Productions <steven.print@btinternet.com>
2025-09-27 14:49:12 +0100

    Add and Spawn the Ground Pick Target Actor

---
+++ Source/Crunch/Public/GAS/TargetActor_GroundPick.h
@@ -0,0 +1,17 @@
+// (c)2025 Ravenscroft Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "TargetActor_GroundPick.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ATargetActor_GroundPick : public AGameplayAbilityTargetActor
+{
+	GENERATED_BODY()
+	
+};
