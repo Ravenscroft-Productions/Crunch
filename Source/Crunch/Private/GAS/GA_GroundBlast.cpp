4006fc1d1f12d39041ef6b29ce24b481d605e7c8
Ravenscroft-Productions <steven.print@btinternet.com>
2025-09-27 16:24:04 +0100

    Find Targets with our Target Actor

--- Source/Crunch/Private/GAS/GA_GroundBlast.cpp
+++ Source/Crunch/Private/GAS/GA_GroundBlast.cpp
@@ -3,6 +3,7 @@
 
 #include "GAS/GA_GroundBlast.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Abilities/Tasks/AbilityTask_WaitTargetData.h"
 #include "GAS/CAbilitySystemStatics.h"
@@ -37,6 +38,13 @@ void UGA_GroundBlast::ActivateAbility(const FGameplayAbilitySpecHandle Handle, c
 
 void UGA_GroundBlast::TargetConfirmed(const FGameplayAbilityTargetDataHandle& TargetDataHandle)
 {
+	TArray<AActor*> TargetActors = UAbilitySystemBlueprintLibrary::GetAllActorsFromTargetData(TargetDataHandle);
+
+	for (AActor* Target : TargetActors)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Found Target: %s"), *Target->GetName());
+	}
+	
 	UE_LOG(LogTemp, Warning, TEXT("Target Confirmed"));
 	K2_EndAbility();
 }
