a7453152b2f2daaac504f13c7651df1e3b8fe95e
Ravenscroft-Productions <steven.print@btinternet.com>
2025-09-28 12:49:32 +0100

    Create the Stats Gauge Class

---
+++ Source/Crunch/Public/Widgets/StatsGauge.h
@@ -0,0 +1,42 @@
+// (c)2025 Ravenscroft Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Blueprint/UserWidget.h"
+#include "StatsGauge.generated.h"
+
+struct FOnAttributeChangeData;
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UStatsGauge : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	UImage* Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AttributeText;
+
+	UPROPERTY(EditAnywhere, Category = "Attribute")
+	FGameplayAttribute Attribute;
+	
+	UPROPERTY(EditAnywhere, Category = "Visual")
+	UTexture2D* IconTexture;
+
+	void SetValue(float NewVal);
+
+	FNumberFormattingOptions NumberFormattingOptions;
+
+	void AttributeChanged(const FOnAttributeChangeData& Data);
+};
