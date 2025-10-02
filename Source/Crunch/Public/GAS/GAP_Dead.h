477d1a289e3196bbb034dd8e113bad357aa93432
Ravenscroft-Productions <steven.print@btinternet.com>
2025-10-02 16:10:37 +0100

    Apply the Rewards

--- Source/Crunch/Public/GAS/GAP_Dead.h
+++ Source/Crunch/Public/GAS/GAP_Dead.h
@@ -37,4 +37,7 @@ private:
 	float KillerRewardPortion = 0.5f;
 	
 	TArray<AActor*> GetRewardTargets() const;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	TSubclassOf<UGameplayEffect> RewardEffect;
 };
