67e4cbb3664283157ea8774c202bee7c8e5e9d7b
Ravenscroft-Productions <steven.print@btinternet.com>
2025-10-03 16:49:15 +0100

    Use Level to drive other Attributes

---
+++ Source/Crunch/Private/GAS/MMC_LevelBased.cpp
@@ -0,0 +1,32 @@
+// (c)2025 Ravenscroft Productions
+
+
+#include "GAS/MMC_LevelBased.h"
+
+#include "GAS/CHeroAttributeSet.h"
+
+UMMC_LevelBased::UMMC_LevelBased()
+{
+	LevelCaptureDefinition.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	LevelCaptureDefinition.AttributeToCapture = UCHeroAttributeSet::GetLevelAttribute();
+
+	RelevantAttributesToCapture.Add(LevelCaptureDefinition);
+}
+
+float UMMC_LevelBased::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	UAbilitySystemComponent* ASC = Spec.GetContext().GetInstigatorAbilitySystemComponent();
+	if (!ASC) return 0.0f;
+
+	float Level = 0.0f;
+	FAggregatorEvaluateParameters EvalParams;
+	EvalParams.SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	EvalParams.TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	GetCapturedAttributeMagnitude(LevelCaptureDefinition, Spec, EvalParams, Level);
+
+	bool bFound;
+	float RateAttributeValue = ASC->GetGameplayAttributeValue(RateAttribute, bFound);
+	if (!bFound) return 0.0f;
+
+	return (Level - 1) * RateAttributeValue;
+}
