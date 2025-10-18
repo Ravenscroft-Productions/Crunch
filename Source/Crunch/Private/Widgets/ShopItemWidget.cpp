d4f6672dccebb967dadcae726fbdac8d4689605c
Ravenscroft-Productions <steven.print@btinternet.com>
2025-10-18 13:29:20 +0100

    Make the TreeNodeInterface and Implement it with ShopItemWidget

--- Source/Crunch/Private/Widgets/ShopItemWidget.cpp
+++ Source/Crunch/Private/Widgets/ShopItemWidget.cpp
@@ -1,19 +1,80 @@
 // (c)2025 Ravenscroft Productions
 
 
+// ReSharper disable CppRedundantQualifier
 #include "Widgets/ShopItemWidget.h"
 
+#include "Components/ListView.h"
+#include "Framework/CAssetManager.h"
 #include "Inventory/PDA_ShopItem.h"
+
+UUserWidget* UShopItemWidget::GetWidget() const
+{
+	UShopItemWidget* Copy = CreateWidget<UShopItemWidget>(GetOwningPlayer(), GetClass());
+	Copy->CopyFromOther(this);
+	return Copy;
+}
+
+TArray<const ITreeNodeInterface*> UShopItemWidget::GetInputs() const
+{
+	const FItemCollection* Collection = UCAssetManager::Get().GetCombinationForItem(GetShopItem());
+	if (Collection) return ItemsToInterfaces(Collection->GetItems());
+
+	return TArray<const ITreeNodeInterface*>();
+}
+
+TArray<const ITreeNodeInterface*> UShopItemWidget::GetOutputs() const
+{
+	const FItemCollection* Collection = UCAssetManager::Get().GetIngredientForItem(GetShopItem());
+	if (Collection) return ItemsToInterfaces(Collection->GetItems());
+
+	return TArray<const ITreeNodeInterface*>();
+}
+
+const UObject* UShopItemWidget::GetItemObject() const
+{
+	return ShopItem;
+}
 
 void UShopItemWidget::NativeOnListItemObjectSet(UObject* ListItemObject)
 {
 	IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+	InitWithShopItem(Cast<UPDA_ShopItem>(ListItemObject));
+	ParentListView = Cast<UListView>(IUserListEntry::GetOwningListView());
+}
 
-	ShopItem = Cast<UPDA_ShopItem>(ListItemObject);
+void UShopItemWidget::CopyFromOther(const UShopItemWidget* OtherWidget)
+{
+	OnItemPurchaseIssued = OtherWidget->OnItemPurchaseIssued;
+	OnShopItemClicked = OtherWidget->OnShopItemClicked;
+	InitWithShopItem(OtherWidget->GetShopItem());
+	ParentListView = OtherWidget->ParentListView;
+}
+
+void UShopItemWidget::InitWithShopItem(const UPDA_ShopItem* NewShopItem)
+{
+	ShopItem = NewShopItem;
 	if (!ShopItem) return;
 	
 	SetIcon(ShopItem->GetIcon());
 	SetToolTipWidget(ShopItem);
+}
+
+TArray<const ITreeNodeInterface*> UShopItemWidget::ItemsToInterfaces(const TArray<const UPDA_ShopItem*>& Items) const
+{
+	TArray<const ITreeNodeInterface*> RetInterfaces;
+	if (!ParentListView) return RetInterfaces;
+
+	for (const UPDA_ShopItem* Item : Items)
+	{
+		const UShopItemWidget* ItemWidget = ParentListView->GetEntryWidgetFromItem<UShopItemWidget>(Item);
+		if (ItemWidget)
+		{
+			RetInterfaces.Add(ItemWidget);
+		}
+	}
+
+	return RetInterfaces;
 }
 
 void UShopItemWidget::RightButtonClicked()
--- Source/Crunch/Public/Widgets/ShopItemWidget.h
+++ Source/Crunch/Public/Widgets/ShopItemWidget.h
@@ -5,8 +5,10 @@
 #include "CoreMinimal.h"
 #include "ItemWidget.h"
 #include "Blueprint/IUserObjectListEntry.h"
+#include "Widgets/TreeNodeInterface.h"
 #include "ShopItemWidget.generated.h"
 
+class UListView;
 class UShopItemWidget;
 class UPDA_ShopItem;
 
@@ -16,19 +18,31 @@ DECLARE_MULTICAST_DELEGATE_OneParam(FOnShopItemSelected, const UShopItemWidget*)
  * 
  */
 UCLASS()
-class CRUNCH_API UShopItemWidget : public UItemWidget, public IUserObjectListEntry
+class CRUNCH_API UShopItemWidget : public UItemWidget, public IUserObjectListEntry, public ITreeNodeInterface
 {
 	GENERATED_BODY()
 public:
 	FOnItemPurchaseIssued OnItemPurchaseIssued;
 	FOnShopItemSelected OnShopItemClicked;
+
+	virtual UUserWidget* GetWidget() const override;
+	virtual TArray<const ITreeNodeInterface*> GetInputs() const override;
+	virtual TArray<const ITreeNodeInterface*> GetOutputs() const override;
+	virtual const UObject* GetItemObject() const override;
 	
 	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
 	FORCEINLINE const UPDA_ShopItem* GetShopItem() const { return ShopItem; }
 
 private:
+	void CopyFromOther(const UShopItemWidget* OtherWidget);
+	void InitWithShopItem(const UPDA_ShopItem* NewShopItem);
+	TArray<const ITreeNodeInterface*> ItemsToInterfaces(const TArray<const UPDA_ShopItem*>& Items) const; 
+	
 	UPROPERTY()
 	const UPDA_ShopItem* ShopItem;
+
+	const UListView* ParentListView;
+	
 	virtual void RightButtonClicked() override;
 	virtual void LeftButtonClicked() override;
 };
