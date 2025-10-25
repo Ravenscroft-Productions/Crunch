a2339db7be21968d302c07329260075753843ff6
Ravenscroft-Productions <steven.print@btinternet.com>
2025-10-25 13:24:56 +0100

    Create the Render Actor Widget Class

---
+++ Source/Crunch/Private/Widgets/RenderActorWidget.cpp
@@ -0,0 +1,79 @@
+// (c)2025 Ravenscroft Productions
+
+
+#include "Widgets/RenderActorWidget.h"
+
+#include "Components/Image.h"
+#include "Components/SizeBox.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Widgets/RenderActor.h"
+
+void URenderActorWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	RenderSizeBox->SetWidthOverride(RenderSize.X);
+	RenderSizeBox->SetHeightOverride(RenderSize.Y);
+}
+
+void URenderActorWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SpawnRenderActor();
+	ConfigureRenderActor();
+	BeginRenderCapture();
+}
+
+void URenderActorWidget::BeginDestroy()
+{
+	StopRenderCapture();
+	Super::BeginDestroy();
+}
+
+void URenderActorWidget::ConfigureRenderActor()
+{
+	if (!GetRenderActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Render Actor, Nothing will be rendered"));
+		return;
+	}
+
+	RenderTarget = NewObject<UTextureRenderTarget2D>(this);
+	RenderTarget->InitAutoFormat(RenderSize.X, RenderSize.Y);
+	RenderTarget->RenderTargetFormat = ETextureRenderTargetFormat::RTF_RGBA8;
+	GetRenderActor()->SetRenderTarget(RenderTarget);
+
+	UMaterialInstanceDynamic* DisplayImageDynamicMaterial = DisplayImage->GetDynamicMaterial();
+	if (DisplayImageDynamicMaterial)
+	{
+		DisplayImageDynamicMaterial->SetTextureParameterValue(DisplayImageRenderTargetParamName, RenderTarget);
+	}
+}
+
+void URenderActorWidget::BeginRenderCapture()
+{
+	RenderTickInterval = 1.0f / (float)FrameRate;
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		World->GetTimerManager().SetTimer(RenderTimerHandle, this, &URenderActorWidget::UpdateRender, RenderTickInterval, true);
+	}
+}
+
+void URenderActorWidget::UpdateRender()
+{
+	if (GetRenderActor())
+	{
+		GetRenderActor()->UpdateRender();
+	}
+}
+
+void URenderActorWidget::StopRenderCapture()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		World->GetTimerManager().ClearTimer(RenderTimerHandle);
+	}
+}
