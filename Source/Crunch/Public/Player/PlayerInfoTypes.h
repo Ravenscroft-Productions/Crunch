ab23c9a8bd49ad03432cf84ef6d8c6faf335b6f7
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-07 09:25:28 +0000

    Create the Player Selection Structure

---
+++ Source/Crunch/Public/Player/PlayerInfoTypes.h
@@ -0,0 +1,35 @@
+// (c)2025 Ravenscroft Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerInfoTypes.generated.h"
+
+USTRUCT()
+struct FPlayerSelection
+{
+	GENERATED_BODY()
+public:
+	FPlayerSelection();
+	FPlayerSelection(uint8 InSlot, const APlayerState* InPlayerState);
+	
+	FORCEINLINE void SetSlot(uint8 NewSlot) { Slot = NewSlot; }
+	FORCEINLINE uint8 GetPlayerSlot() const { return Slot; }
+	FORCEINLINE FUniqueNetIdRepl GetPlayerUniqueId() const { return PlayerUniqueId; }
+	FORCEINLINE FString GetPlayerNickName() const { return PlayerNickName; }
+	
+	bool IsForPlayer(const APlayerState* PlayerState) const;
+	bool IsValid() const;
+	
+	static uint8 GetInvalidSlot();
+	
+private:
+	UPROPERTY()
+	uint8 Slot;
+	
+	UPROPERTY()
+	FUniqueNetIdRepl PlayerUniqueId;
+	
+	UPROPERTY()
+	FString PlayerNickName;
+};
