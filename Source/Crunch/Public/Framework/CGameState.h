0f255621b7cc3a7b3c01026b0c67db584f233d2a
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-07 09:54:59 +0000

    Make the CGameState class to keep the Player Selections

---
+++ Source/Crunch/Public/Framework/CGameState.h
@@ -0,0 +1,32 @@
+// (c)2025 Ravenscroft Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "Player/PlayerInfoTypes.h"
+#include "CGameState.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerSelectionUpdated, const TArray<FPlayerSelection>& /*NewPlayerSelection*/);
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ACGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+public:
+	FOnPlayerSelectionUpdated OnPlayerSelectionUpdated;
+	
+	void RequestPlayerSelectionChange(const APlayerState* RequestingPlayer, uint8 DesiredSlot);
+	bool IsSlotOccupied(uint8 SlotId) const;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	const TArray<FPlayerSelection>& GetPlayerSelection() const;
+	
+private:
+	UPROPERTY(ReplicatedUsing = OnRep_PlayerSelectionArray)
+	TArray<FPlayerSelection> PlayerSelectionArray;
+	
+	UFUNCTION()
+	void OnRep_PlayerSelectionArray();
+};
