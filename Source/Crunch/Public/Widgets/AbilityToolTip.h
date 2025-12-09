05c7c245744c05f6ab8416652b3a442525449a79
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-09 12:50:18 +0000

    Create and Configure the Ability Tool Tip

---
+++ Source/Crunch/Public/Widgets/AbilityToolTip.h
@@ -0,0 +1,36 @@
+// (c)2025 Ravenscroft Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityToolTip.generated.h"
+
+class UImage;
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UAbilityToolTip : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void SetAbilityInfo(const FName& AbilityName, UTexture2D* AbilityTexture, const FText& AbilityDescription, float AbilityCooldown, float AbilityCost);
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityNameText;
+	
+	UPROPERTY(meta=(BindWidget))
+	UImage* AbilityIcon;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityDescriptionText;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityCooldownText;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityCostText;
+};
